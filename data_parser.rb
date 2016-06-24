5c28c8a12f7f9ce0a7dc7cad42d191e06e28defd
Marie Denney <mldenne@gmail.com>
2016-06-24 11:54:23 -0400

    Added changes

--- data_parser.rb
+++ data_parser.rb
@@ -7,11 +7,45 @@ hashed_data = data.map{|d| d.to_hash}
 
 class Delivery
 
-  attr_accessor :destination :shipments :crates :money :pilot :bonus
+  attr_accessor :destination, :shipment, :crates, :money, :pilot, :bonus
 
   def initialize(hash)
     @destination = hash[:destination]
-    @shipments = hash[:shipments]
-    @crates = hash[:shipments]
+    @shipment = hash[:shipment]
+    @crates = hash[:crates]
     @money = hash[:money]
+    determine_pilot
+    determine_bonus
   end
+
+  def determine_pilot
+    if destination == "Earth"
+      self.pilot = "Fry"
+    elsif destination == "Mars"
+      self.pilot = "Amy"
+    elsif destination == "Uranus"
+      self.pilot = "Bender"
+    else
+      self.pilot = "Leela"
+    end
+  end
+
+  def determine_bonus
+    self.bonus = money.to_f * 0.10
+  end
+
+end
+
+deliveries = hashed_data.map {|x| Delivery.new(x)}
+
+total_sales = deliveries.select{|x| x.money}.inject(:+)
+
+fry_bonus = deliveries.select{|x| x.pilot == "Fry"}.collect{|y| y.bonus}.inject(:+)
+amy_bonus = deliveries.select{|x| x.pilot == "Amy"}.collect{|y| y.bonus}.inject(:+)
+bender_bonus = deliveries.select{|x| x.pilot} == "Bender".collect{|y| y.bonus}.inject(:+)
+leela_bonus = deliveries.select{|x| x.pilot} == "Leela".collect{|y| y.bonus}.inject(:+)
+
+fry_trips = deliveries.count{|x| x.pilot == "Fry"}
+amy_trips = deliveries.count{|x| x.pilot == "Amy"}
+bender_trips = deliveries.count{|x| x.pilot == "Bender"}
+leela_trips = deliveries.count{|x| x.pilot == "Leela"}
