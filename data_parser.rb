7a24ef4f8c8b9ef867931d0fa6bb7dbf7f52b98c
Marie Denney <mldenne@gmail.com>
2016-06-26 20:39:13 -0400

    Final commit

--- data_parser.rb
+++ data_parser.rb
@@ -1,11 +1,12 @@
 require 'csv'
 require 'erb'
 
+# import of Planet Express data files
 data = CSV.read("planet_express_logs.csv", {headers: true, header_converters: :symbol, converters: :all})
 
 hashed_data = data.map{|d| d.to_hash}
 
-
+# creation of Delivery class
 class Delivery
 
   attr_accessor :destination, :shipment, :crates, :money, :pilot, :bonus
@@ -37,25 +38,30 @@ class Delivery
 
 end
 
+# creation of deliveries object by Delivery class
 deliveries = hashed_data.map {|x| Delivery.new(x)}
 
 total_sales = deliveries.map{|x| x.money}.inject(:+)
 
+# total earnings for each pilot
 fry_earned = deliveries.select{|x| x.pilot == "Fry"}.collect{|y| y.money}.inject(:+)
 amy_earned = deliveries.select{|x| x.pilot == "Amy"}.collect{|y| y.money}.inject(:+)
 bender_earned = deliveries.select{|x| x.pilot == "Bender"}.collect{|y| y.money}.inject(:+)
 leela_earned = deliveries.select{|x| x.pilot == "Leela"}.collect{|y| y.money}.inject(:+)
 
+# bonus calculation for each pilot
 fry_bonus = deliveries.select{|x| x.pilot == "Fry"}.collect{|y| y.bonus}.inject(:+)
 amy_bonus = deliveries.select{|x| x.pilot == "Amy"}.collect{|y| y.bonus}.inject(:+)
 bender_bonus = deliveries.select{|x| x.pilot == "Bender"}.collect{|y| y.bonus}.inject(:+)
 leela_bonus = deliveries.select{|x| x.pilot == "Leela"}.collect{|y| y.bonus}.inject(:+)
 
+# number of trips each pilot made
 fry_trips = deliveries.count{|x| x.pilot == "Fry"}
 amy_trips = deliveries.count{|x| x.pilot == "Amy"}
 bender_trips = deliveries.count{|x| x.pilot == "Bender"}
 leela_trips = deliveries.count{|x| x.pilot == "Leela"}
 
+# output to html file
 new_file = File.open("./report.html", "w+")
 new_file << ERB.new(File.read("report.erb")).result(binding)
 new_file.close
